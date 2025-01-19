4829e52b329d3e243516b60558cec0d1cd584196
Fábio de Lima <fabiolima@github.com>
2025-01-19 16:13:12 -0300

    refatora downloader

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 require "yaml"
 require "./src/downloader"
 
-download_config = YAML.load_file('./src/config/download.yaml').transform_keys!(&:to_sym)
-Downloader.new(**download_config).start
+HTTParty::Response.class_eval do
+  def warn_about_nil_deprecation
+  end
+end
+
+# download_config = YAML.load_file('./src/config/download.yaml').transform_keys!(&:to_sym)
+
+Downloader.new.start
 
 # ImporterManager.new.start
