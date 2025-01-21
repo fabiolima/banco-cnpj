00be4295b1b1b8c6b40c337bce4e6bf48e031c54
Fábio de Lima <fabiolima@github.com>
2025-01-21 12:52:19 -0300

    estabelecimentos ok

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require "yaml"
 require "./src/downloader"
+require_relative "src/importer_manager"
 
 HTTParty::Response.class_eval do
   def warn_about_nil_deprecation
@@ -8,6 +9,6 @@ end
 
 # download_config = YAML.load_file('./src/config/download.yaml').transform_keys!(&:to_sym)
 
-Downloader.new.start
+# Downloader.new.start
 
-# ImporterManager.new.start
+ImporterManager.new.start
