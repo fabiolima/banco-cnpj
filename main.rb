84cd5d48ce8fc694e42fb729cce15d6a17bc4bdf
Fábio de Lima <fabiolima@github.com>
2025-01-23 16:29:19 -0300

    cleaning

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
-require "yaml"
-require "./src/downloader"
+require_relative "src/downloader"
 require_relative "src/importer_manager"
 
+# Stop annoying warning
+# See https://github.com/jnunemaker/httparty/issues/568
 HTTParty::Response.class_eval do
   def warn_about_nil_deprecation
   end
 end
 
-# download_config = YAML.load_file('./src/config/download.yaml').transform_keys!(&:to_sym)
-
-# Downloader.new.start
-
+Downloader.new.start
 ImporterManager.new.start
